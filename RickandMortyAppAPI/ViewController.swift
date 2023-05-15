bf547c80b67c1d789f894c54331d247e254eeb1b
polosa00 <i@apolochanin.ru>
2023-05-15 22:52:19 +0300

    Initial Commit

---
+++ RickandMortyAppAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMortyAppAPI
+//
+//  Created by Александр Полочанин on 15.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
